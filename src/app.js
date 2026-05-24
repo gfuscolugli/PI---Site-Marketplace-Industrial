076bc607b72bebca761d3289370d52110ec906c5
Guilherme Fusco <gfusketa@gmail.com>
2026-05-24 10:49:56 -0300

    Fix: Atualiza configuracao do CORS para liberar rotas do Front-end

--- src/app.js
+++ src/app.js
@@ -4,19 +4,21 @@ const routes = require('./routes');
 
 const app = express();
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200
-};
+// Configuração robusta do CORS adicionada aqui
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Bem vindo à API Revalor Marketplace.' });
+    res.json({ message: 'Bem vindo à API Revalor Marketplace.' });
 });
 
 module.exports = app;
