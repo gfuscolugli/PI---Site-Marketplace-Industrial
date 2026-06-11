96052f7183458fea4025590d152e522d9a201177
Gabriel Lippi <gabilippiangelini@gmail.com>
2026-06-11 20:04:55 -0300

    Build: Reset completo da main para reestruturação

--- src/components/container/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from 'react'
-export function Container({children}: { children: ReactNode}){
-  return(
-    <div className="w-full max-w-7x1 mx-auto px-4">
-      {children}
-    </div>
-  )
-}
